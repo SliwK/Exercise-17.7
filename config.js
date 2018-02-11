1411761feb0697d1228532eba76cc3e8b3c18c89
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-02-11 18:42:50 +0100

    Create project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': "829469687444-lq2ho5qq39rhok16igqmbep2oi8lcl78.apps.googleusercontent.com",
+  "GOOGLE_CLIENT_SECRET": "W3vhvVtYoHzTLbHQK26e8DyF",
+  "CALLBACK_URL": "http://localhost:3000/auth/google/callback"
+};
